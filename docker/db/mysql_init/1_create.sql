d694fdd067c94731bf38f4c49b6d90b8bc6ca620
tatsuhiro <tatsuhiro.0906@gmail.com>
2020-06-26 23:14:44 +0900

    init

--- docker/db/mysql_init/1_create.sql
+++ docker/db/mysql_init/1_create.sql
@@ -8,3 +8,13 @@ CREATE TABLE articles(
     tag VARCHAR(64) NOT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE works(
+    id INT(11) AUTO_INCREMENT NOT NULL ,
+    title VARCHAR(64) NOT NULL ,
+    discription TEXT NOT NULL ,
+    langages VARCHAR(64) NOT NULL ,
+    tools VARCHAR(64) NOT NULL,
+    links VARCHAR(64) ,
+    
+)
